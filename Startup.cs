eff95ff9f28fbf22f7643b11a0b2bd4815751e1e
Muhammed Ubeyd <muhammedubeyd101112@gmail.com>
2021-11-18 17:55:41 +0300

    adding json packs for patch requests

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -28,7 +29,10 @@ namespace Commander
                 );
 
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
